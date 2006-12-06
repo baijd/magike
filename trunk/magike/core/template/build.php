f7a014f0e88f8719d75b8f48e062cc8c6e8726e2
magike.net <magike.net@a2e7fb70-2023-0410-8fa0-b502d334031e>
2006-12-06 06:21:08 +0000

    增加array_intersect_key函数

--- trunk/magike/core/template/build.php
+++ trunk/magike/core/template/build.php
@@ -5,4 +5,12 @@
  * Copyright : Magike Group
  * License   : GNU General Public License 2.0
  *********************************/
+
+class Build extends MagikeObject
+{
+	function __construct($templateFile,$template)
+	{
+
+	}
+}
 ?>
