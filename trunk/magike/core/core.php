7061d47c615dd0d530453f3dfcc2d0c2f6a1b3b3
magike.net <magike.net@a2e7fb70-2023-0410-8fa0-b502d334031e>
2007-04-26 06:54:01 +0000

    更新文件名

--- trunk/magike/core/core.php
+++ trunk/magike/core/core.php
@@ -9,7 +9,7 @@ define('__DIR__',dirname(__FILE__));			//框架绝对路径
 require(__DIR__.'/core.config.php');
 require(__CONFIG__);
 require(__DIR__.'/core.functions.php');
-require(__DIR__.'/core.hook.php');
+require(__DIR__.'/core.init.php');
 
 //初始化动作
 require(__DIR__.'/action/action.php');
